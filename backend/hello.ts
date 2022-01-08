30cc7da24bca7a0a5d4f4d91865c4dbd61b6aba3
Nathan Chandler-Smith <nate.chandlersmith@gmail.com>
2022-01-07 20:12:53 -0600

    crude hello appears to work

---
+++ backend/hello.ts
@@ -0,0 +1,13 @@
+import 'source-map-support/register'
+
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
+
+export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+        message: 'Hello!',
+        input: event,
+      })
+  };
+};
