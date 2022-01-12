d760baa4968fe9c174223cedbb7d54babfe83fd3
Nathan Chandler-Smith <nate.chandlersmith@gmail.com>
2022-01-11 19:58:00 -0600

    fixes indentation issues

--- backend/src/createAccount.spec.ts
+++ backend/src/createAccount.spec.ts
@@ -1,6 +1,5 @@
 import AWSMock from 'aws-sdk-mock'
 import AWS from 'aws-sdk'
-import sinon from 'ts-sinon'
 import { PutItemInput } from 'aws-sdk/clients/dynamodb'
 import { APIGatewayEventDefaultAuthorizerContext, APIGatewayEventIdentity, APIGatewayEventRequestContextWithAuthorizer, APIGatewayProxyEvent } from "aws-lambda"
 import { handler } from "./createAccount"
@@ -8,38 +7,38 @@ import { CreateAccountRequest, CreateAccountResponse, CreateAccountDao } from ".
 
 describe('createAccount', () => {
 
-    beforeEach(() => {
-      AWSMock.setSDKInstance(AWS)
+  beforeEach(() => {
+    AWSMock.setSDKInstance(AWS)
+  })
+
+  afterEach(() => {
+    AWSMock.restore('DynamoDB.DocumentClient')
+  })
+
+  it('should return account information', async () => {
+    const accountType = 'Checking'
+    const initialDeposit = 1000
+    const request = buildRequestBody(accountType, initialDeposit)
+
+    const response = await handler(buildEvent(JSON.stringify(request)))
+    
+    expect(response.statusCode).toEqual(201)
+    const accountInfo: CreateAccountResponse = JSON.parse(response.body)?.accountInfo
+    expect(accountInfo.accountId).not.toBeNull()
+    expect(accountInfo.accountType).toEqual(accountType)
+    expect(accountInfo.balance).toEqual(initialDeposit)
+  })
+
+  it('should do something with the mock', async () => {
+    AWSMock.mock('DynamoDB.DocumentClient', 'put', (params: CreateAccountDao, callback: Function) => {
+    console.log(' Doc client called!!!')
+    callback(null, {Foo: 'bar'})
     })
 
-    afterEach(() => {
-      AWSMock.restore('DynamoDB.DocumentClient')
-    })
-
-    it('should return account information', async () => {
-      const accountType = 'Checking'
-      const initialDeposit = 1000
-      const request = buildRequestBody(accountType, initialDeposit)
-
-      const response = await handler(buildEvent(JSON.stringify(request)))
-      
-      expect(response.statusCode).toEqual(201)
-      const accountInfo: CreateAccountResponse = JSON.parse(response.body)?.accountInfo
-      expect(accountInfo.accountId).not.toBeNull()
-      expect(accountInfo.accountType).toEqual(accountType)
-      expect(accountInfo.balance).toEqual(initialDeposit)
-    })
-
-    it('should do something with the mock', async () => {
-      AWSMock.mock('DynamoDB.DocumentClient', 'put', (params: CreateAccountDao, callback: Function) => {
-      console.log(' Doc client called!!!')
-      callback(null, {Foo: 'bar'})
-      })
-
-      const input: PutItemInput = {TableName: '', Item: {'Foo': {S: 'bar'}}}
-      const client = new AWS.DynamoDB.DocumentClient({apiVersion: '2012-08-10'})
-      expect(await client.put(input).promise()).toStrictEqual({Foo: 'bar'})
-    })
+    const input: PutItemInput = {TableName: '', Item: {'Foo': {S: 'bar'}}}
+    const client = new AWS.DynamoDB.DocumentClient({apiVersion: '2012-08-10'})
+    expect(await client.put(input).promise()).toStrictEqual({Foo: 'bar'})
+  })
 
 function buildRequestBody(accountType: string, initialDeposit: number): CreateAccountRequest {
   return {
@@ -49,20 +48,20 @@ function buildRequestBody(accountType: string, initialDeposit: number): CreateAc
 }
 
 function buildEvent(body: string): APIGatewayProxyEvent {
-    return {
-        resource: '',
-        path: '',
-        httpMethod: '',
-        headers: {},
-        multiValueHeaders: {},
-        queryStringParameters: null,
-        multiValueQueryStringParameters: null,
-        pathParameters: null,
-        stageVariables: null,
-        requestContext: buildRequestContext(),
-        body,
-        isBase64Encoded: false
-    }
+  return {
+    resource: '',
+    path: '',
+    httpMethod: '',
+    headers: {},
+    multiValueHeaders: {},
+    queryStringParameters: null,
+    multiValueQueryStringParameters: null,
+    pathParameters: null,
+    stageVariables: null,
+    requestContext: buildRequestContext(),
+    body,
+    isBase64Encoded: false
+  }
 }
 
 function buildRequestContext(): APIGatewayEventRequestContextWithAuthorizer<APIGatewayEventDefaultAuthorizerContext> {
