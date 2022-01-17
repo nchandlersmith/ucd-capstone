4cc941e2e591dfd56e2740f0812171508beb5773
Nathan Chandler-Smith <nate.chandlersmith@gmail.com>
2022-01-16 19:06:15 -0600

    creates account

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
+import Button from 'react-bootstrap/Button'
+import { Form } from 'react-bootstrap';
 
 function App() {
+  const [accountType, setAccountType] = useState('')
+  const [initialDeposit, setInitialDeposit] = useState(0)
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Form>
+        <Form.Group controlId='accountType'>
+          <Form.Label>Account Type</Form.Label>
+          <Form.Control 
+            type='text'
+            placeholder='account type' 
+            onChange={(event) => setAccountType(event.currentTarget.value)}/>
+          <Form.Text>Enter account type.</Form.Text>
+        </Form.Group>
+        <Form.Group controlId='initialDeposit'>
+          <Form.Label>Initial Deposit</Form.Label>
+          <Form.Control
+            type='text'
+            placeholder='0'
+            onChange={(event) => setInitialDeposit(parseFloat(event.currentTarget.value))}/>
+          <Form.Text>Enter initial depsoit in dollars.</Form.Text>
+        </Form.Group>
+        <Button variant='primary' type='reset' onClick={post}>
+          Submit
+        </Button>
+      </Form>
     </div>
-  );
+  )
+
+async function post() {
+  const url = 'https://a6gx2186x5.execute-api.us-east-1.amazonaws.com/dev/accounts'
+  console.log(url)
+  const data = {
+    accountType,
+    initialDeposit
+  }
+  axios.post(url, data)
+  .then(response => {
+    console.log(`response: JSON.stringify(response)`)
+  })
+  .catch(error => {
+    console.error(`error: JSON.stringify(error)`)
+  })
+}
 }
 
 export default App;
