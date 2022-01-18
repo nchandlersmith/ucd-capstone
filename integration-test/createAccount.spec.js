c51120440f6c9c4e737d949440731a354e8e7df0
Nathan Chandler-Smith <nate.chandlersmith@gmail.com>
2022-01-18 15:54:05 -0600

    create account: adds test for missing accountType from create account request

--- integration-test/createAccount.spec.js
+++ integration-test/createAccount.spec.js
@@ -8,37 +8,60 @@ const docClient = new AWS.DynamoDB.DocumentClient({
     secretAccessKey: 'some secret'
   })
 const tableName = 'Accounts-dev'
+const userId = 'Ghost Rider'
 
 describe('create account', () => {
+
+    afterEach(async () => {
+        const dynamoResponse = await findAllAccountsForUserId()
+
+        dynamoResponse.Items.forEach(async (item) => {
+            await deleteAllAccountsByAccountIdForUserId(item.accountId)
+                .catch(error => console.log`Error occured while cleaning up dynamo: ${error.message}`)
+        });
+
+    })
+
     it('should create an account', async () => {
         const url = 'http://localhost:3000/dev/accounts'
         const createAccountData = {
             'accountType': 'Integration Test Account',
             'initialDeposit': 3145
         }
-
-        const result = await axios.post(url, createAccountData)
-        const dynamoItems = await docClient.query({
-            TableName: tableName,
-            ExpressionAttributeValues: {
-                ':userId': 'Ghost Rider'
-            },
-            KeyConditionExpression: 'userId = :userId'
-        }).promise()
-
-        console.log(`items: ${JSON.stringify(dynamoItems.Items)}`)
-        console.log(`the thing: ${dynamoItems.Items[0].userId}`)
-        console.log(`number of things: ${dynamoItems.Items.length}`)
-
-        expect(result.status).toEqual(201)
         
-        await docClient.delete({
-            TableName: tableName,
-            Key: {
-                "userId": dynamoItems.Items[0].userId,
-                "accountId": dynamoItems.Items[0].accountId
-            }
-        }).promise()
+        const result = await axios.post(url, createAccountData)
+        
+        expect(result.status).toEqual(201)
+    })
+    
+    it('should reject requests when accountType is missing', async () => {
+    const url = 'http://localhost:3000/dev/accounts'
+    const createAccountData = {
+        'initialDeposit': 3145
+    }
 
+    const result =  await axios.post(url, createAccountData).catch(error => error)
+        
+    expect(result.response.status).toEqual(400)
     })
 })
+
+function deleteAllAccountsByAccountIdForUserId(accountId) {
+    return docClient.delete({
+        TableName: tableName,
+        Key: {
+            "userId": userId,
+            "accountId": accountId
+        }
+    }).promise()
+}
+
+function findAllAccountsForUserId() {
+    return docClient.query({
+        TableName: tableName,
+        ExpressionAttributeValues: {
+            ':userId': userId
+        },
+        KeyConditionExpression: 'userId = :userId'
+    }).promise()
+}
