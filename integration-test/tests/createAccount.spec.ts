f32a94a4395276aceb2562a242a8d7dfebc6a03b
Nathan Chandler-Smith <nate.chandlersmith@gmail.com>
2022-01-23 10:32:27 -0600

    Get Account: Refactors

--- integration-test/tests/createAccount.spec.ts
+++ integration-test/tests/createAccount.spec.ts
@@ -8,9 +8,9 @@ const userId = 'Ghost Rider'
 describe('create account', () => {
   const userId = 'Ghost Rider'
   const headers = {
-    withCredentials: true,
     Authorization: `Bearer blarg-${userId}`
   }
+  const accountsUrl = 'http://localhost:3000/dev/accounts'
 
   afterEach(async () => {
     const dynamoResponse = await findAllAccountsForUserId()
@@ -23,13 +23,12 @@ describe('create account', () => {
   })
 
   it('should create an account', async () => {
-    const url = 'http://localhost:3000/dev/accounts'
     const createAccountData = {
       'accountType': 'Integration Test',
       'initialDeposit': 3145
     }
 
-    const result = await axios.post(url, createAccountData, {headers})
+    const result = await axios.post(accountsUrl, createAccountData, {headers})
 
     const dynamoResponse = await findAllAccountsForUserId()
     expect(result.status).toEqual(201)
@@ -41,13 +40,12 @@ describe('create account', () => {
   })
 
   it('should reject requests when accountType is missing', async () => {
-    const url = 'http://localhost:3000/dev/accounts'
     const createAccountData = {
       'initialDeposit': 3145
     }
     const expectedErrorMessage = 'Missing from body: accountType'
 
-    const result =  await axios.post(url, createAccountData, {headers}).catch((error: any) => error)
+    const result =  await axios.post(accountsUrl, createAccountData, {headers}).catch((error: any) => error)
 
     const dynamoResponse = await findAllAccountsForUserId()
     expect(result.response.status).toEqual(400)
@@ -56,13 +54,12 @@ describe('create account', () => {
   })
 
   it('should reject requests when initialDeposit is missing', async () => {
-    const url = 'http://localhost:3000/dev/accounts'
     const createAccountData = {
       'accountType': 'Another integration test'
     }
     const expectedErrorMessage = 'Missing from body: initialDeposit'
 
-    const result =  await axios.post(url, createAccountData, {headers}).catch((error: any) => error)
+    const result =  await axios.post(accountsUrl, createAccountData, {headers}).catch((error: any) => error)
 
     const dynamoResponse = await findAllAccountsForUserId()
     expect(result.response.status).toEqual(400)
