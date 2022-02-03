f57b555bdedb5a5062d3642135a20e73766bde4a
Nathan Chandler-Smith <nate.chandlersmith@gmail.com>
2022-02-02 21:43:56 -0600

    refactors

--- backend/src/lambdas/getAccountsLambda.ts
+++ backend/src/lambdas/getAccountsLambda.ts
@@ -3,6 +3,7 @@ import {getAccountsByUser} from "../persistence/dbClient";
 import {createLogger} from "../utils/logger";
 import {authorize} from "../utils/authUtils";
 import {errorResponseBuilder, responseBuilder} from "../utils/responseUtils";
+import {CapstoneAccount} from "../models/getAccountsModels";
 
 const logger = createLogger('Get Account')
 
@@ -10,11 +11,16 @@ export const handler = async function(event: APIGatewayProxyEvent): Promise<APIG
   const authHeader = event.headers.Authorization
   try {
     const user = authorize(authHeader)
-    const dbResult = await getAccountsByUser(user)
-    return responseBuilder(200, {accounts: dbResult.Items})
+    const accounts = await getUserAccounts(user)
+    return responseBuilder(200, {accounts})
   } catch(err) {
     logger.info(`Auth error with Authorization header: ${authHeader}`)
     const error = err as Error
     return errorResponseBuilder(error)
   }
+}
+
+async function getUserAccounts(user: string): Promise<CapstoneAccount[]> {
+  const dbResult = await getAccountsByUser(user)
+  return dbResult.Items as CapstoneAccount[]
 }
