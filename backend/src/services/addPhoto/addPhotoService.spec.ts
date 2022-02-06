b890f70cb7a6123c50daa721fc71043a0eb334c7
Nathan Chandler-Smith <nate.chandlersmith@gmail.com>
2022-02-05 21:47:13 -0600

    refactors service spec

--- backend/src/services/addPhoto/addPhotoService.spec.ts
+++ backend/src/services/addPhoto/addPhotoService.spec.ts
@@ -3,85 +3,84 @@ import {AddPhotoRequest} from "../../models/addPhotoModels"
 import {ModelValidationError} from "../../exceptions/exceptions"
 
 describe("addPhotosService", () => {
-  describe("email validation", () => {
-    const request: AddPhotoRequest = {
-      emailAddress: "string@string.string",
-      label: "photo label",
-      service: "selected service",
-      vendor: "selected vendor"
-    }
+  const request: AddPhotoRequest = {
+    emailAddress: "string@string.string",
+    label: "photo label",
+    service: "selected service",
+    vendor: "selected vendor"
+  }
 
+  describe("email validation", () => {
     it("should throw error when emailAddress is null", () => {
-      const nullEmail = {...request, emailAddress: null}
+      const requestWithBadEmail = {...request, emailAddress: null}
       // @ts-ignore
-      expect(() => addPhoto(nullEmail)).toThrow(ModelValidationError)
+      expect(() => addPhoto(requestWithBadEmail)).toThrow(ModelValidationError)
       // @ts-ignore
-      expect(() => addPhoto(nullEmail)).toThrow(/^Email address invalid. Request denied.$/)
+      expect(() => addPhoto(requestWithBadEmail)).toThrow(/^Email address invalid. Request denied.$/)
     })
 
     it("should throw error when emailAddress is undefined", () => {
-      const nullEmail = {...request, emailAddress: undefined}
+      const requestWithBadEmail = {...request, emailAddress: undefined}
       // @ts-ignore
-      expect(() => addPhoto(nullEmail)).toThrow(ModelValidationError)
+      expect(() => addPhoto(requestWithBadEmail)).toThrow(ModelValidationError)
       // @ts-ignore
-      expect(() => addPhoto(nullEmail)).toThrow(/^Email address invalid. Request denied.$/)
+      expect(() => addPhoto(requestWithBadEmail)).toThrow(/^Email address invalid. Request denied.$/)
     })
 
     it("should throw error when emailAddress is empty", () => {
-      const nullEmail = {...request, emailAddress: ""}
+      const requestWithBadEmail = {...request, emailAddress: ""}
       // @ts-ignore
-      expect(() => addPhoto(nullEmail)).toThrow(ModelValidationError)
+      expect(() => addPhoto(requestWithBadEmail)).toThrow(ModelValidationError)
       // @ts-ignore
-      expect(() => addPhoto(nullEmail)).toThrow(/^Email address invalid. Request denied.$/)
+      expect(() => addPhoto(requestWithBadEmail)).toThrow(/^Email address invalid. Request denied.$/)
     })
 
     it("should throw error when emailAddress local part is missing", () => {
-      const nullEmail = {...request, emailAddress: "@string.string"}
+      const requestWithBadEmail = {...request, emailAddress: "@string.string"}
       // @ts-ignore
-      expect(() => addPhoto(nullEmail)).toThrow(ModelValidationError)
+      expect(() => addPhoto(requestWithBadEmail)).toThrow(ModelValidationError)
       // @ts-ignore
-      expect(() => addPhoto(nullEmail)).toThrow(/^Email address invalid. Request denied.$/)
+      expect(() => addPhoto(requestWithBadEmail)).toThrow(/^Email address invalid. Request denied.$/)
     })
 
     it("should throw error when emailAddress username is missing", () => {
-      const nullEmail = {...request, emailAddress: "@string.string"}
+      const requestWithBadEmail = {...request, emailAddress: "@string.string"}
       // @ts-ignore
-      expect(() => addPhoto(nullEmail)).toThrow(ModelValidationError)
+      expect(() => addPhoto(requestWithBadEmail)).toThrow(ModelValidationError)
       // @ts-ignore
-      expect(() => addPhoto(nullEmail)).toThrow(/^Email address invalid. Request denied.$/)
+      expect(() => addPhoto(requestWithBadEmail)).toThrow(/^Email address invalid. Request denied.$/)
     })
 
     it("should throw error when emailAddress @ is missing", () => {
-      const nullEmail = {...request, emailAddress: "string.string"}
+      const requestWithBadEmail = {...request, emailAddress: "string.string"}
       // @ts-ignore
-      expect(() => addPhoto(nullEmail)).toThrow(ModelValidationError)
+      expect(() => addPhoto(requestWithBadEmail)).toThrow(ModelValidationError)
       // @ts-ignore
-      expect(() => addPhoto(nullEmail)).toThrow(/^Email address invalid. Request denied.$/)
+      expect(() => addPhoto(requestWithBadEmail)).toThrow(/^Email address invalid. Request denied.$/)
     })
 
     it("should throw error when domain name is missing", () => {
-      const nullEmail = {...request, emailAddress: "string@.string"}
+      const requestWithBadEmail = {...request, emailAddress: "string@.string"}
       // @ts-ignore
-      expect(() => addPhoto(nullEmail)).toThrow(ModelValidationError)
+      expect(() => addPhoto(requestWithBadEmail)).toThrow(ModelValidationError)
       // @ts-ignore
-      expect(() => addPhoto(nullEmail)).toThrow(/^Email address invalid. Request denied.$/)
+      expect(() => addPhoto(requestWithBadEmail)).toThrow(/^Email address invalid. Request denied.$/)
     })
 
     it("should throw error when . is missing", () => {
-      const nullEmail = {...request, emailAddress: "string@string"}
+      const requestWithBadEmail = {...request, emailAddress: "string@string"}
       // @ts-ignore
-      expect(() => addPhoto(nullEmail)).toThrow(ModelValidationError)
+      expect(() => addPhoto(requestWithBadEmail)).toThrow(ModelValidationError)
       // @ts-ignore
-      expect(() => addPhoto(nullEmail)).toThrow(/^Email address invalid. Request denied.$/)
+      expect(() => addPhoto(requestWithBadEmail)).toThrow(/^Email address invalid. Request denied.$/)
     })
 
     it("should throw error when domain is missing", () => {
-      const nullEmail = {...request, emailAddress: "string@string."}
+      const requestWithBadEmail = {...request, emailAddress: "string@string."}
       // @ts-ignore
-      expect(() => addPhoto(nullEmail)).toThrow(ModelValidationError)
+      expect(() => addPhoto(requestWithBadEmail)).toThrow(ModelValidationError)
       // @ts-ignore
-      expect(() => addPhoto(nullEmail)).toThrow(/^Email address invalid. Request denied.$/)
+      expect(() => addPhoto(requestWithBadEmail)).toThrow(/^Email address invalid. Request denied.$/)
     })
-
   })
 })
