e898248b8aa318868e98d205c56d04388f1d814e
Nathan Chandler-Smith <nate.chandlersmith@gmail.com>
2022-02-19 19:26:10 -0600

    adds sad path tests to lambda

--- backend/src/lambdas/addPhoto/addPhotoLambda.spec.ts
+++ backend/src/lambdas/addPhoto/addPhotoLambda.spec.ts
@@ -3,9 +3,16 @@ import {handler} from "./addPhotoLambda";
 import {buildEvent} from "../../testUtils/eventUtils";
 
 const putSignedUrl = "https://put.com"
-jest.mock("../../services/addPhoto/addPhotoService", () => {
+jest.mock("../../persistence/s3Client", () => {
   return {
-    addPhoto: () => {return putSignedUrl}
+    createGetSignedUrl: () => Promise.resolve(),
+    createPutSignedUrl: () => Promise.resolve(putSignedUrl)
+  }
+})
+
+jest.mock("../../persistence/dbClient", () => {
+  return {
+    insertPhoto: () => Promise.resolve()
   }
 })
 
@@ -20,12 +27,57 @@ describe("getPhotoLambda", () => {
     "access-control-allow-origin": "*"
   }
 
-  it("should return success", async () => {
+  it("should return put signed url", async () => {
     const expectedResponseBody = JSON.stringify({putPhotoSignedUrl: putSignedUrl})
 
-    const response = await handler(buildEvent({body: JSON.stringify(request)}))
-    expect(response.statusCode).toEqual(201)
-    expect(response.headers).toStrictEqual(expectedHeaders)
-    expect(response.body).toStrictEqual(expectedResponseBody)
+    const result = await handler(buildEvent({body: JSON.stringify(request)}))
+
+    expect(result.statusCode).toEqual(201)
+    expect(result.headers).toStrictEqual(expectedHeaders)
+    expect(result.body).toStrictEqual(expectedResponseBody)
+  })
+
+  it("should return 400 when the email address is missing", async () => {
+    const {emailAddress, ...requestWithInvalidEmailAddress} = request
+    const expectedBody = JSON.stringify({error: "Add photo request is missing emailAddress. Request denied."})
+
+    const result = await handler(buildEvent({body: JSON.stringify(requestWithInvalidEmailAddress)}))
+
+    expect(result.statusCode).toEqual(400)
+    expect(result.headers).toStrictEqual(expectedHeaders)
+    expect(result.body).toStrictEqual(expectedBody)
+  })
+
+  it("should return 400 when the label is missing", async () => {
+    const {label, ...requestWithInvalidLabel} = request
+    const expectedBody = JSON.stringify({error: "Add photo request is missing label. Request denied."})
+
+    const result = await handler(buildEvent({body: JSON.stringify(requestWithInvalidLabel)}))
+
+    expect(result.statusCode).toEqual(400)
+    expect(result.headers).toStrictEqual(expectedHeaders)
+    expect(result.body).toStrictEqual(expectedBody)
+  })
+
+  it("should return 400 when the service is missing", async () => {
+    const {service, ...requestWithMissingService} = request
+    const expectedBody = JSON.stringify({error: "Add photo request is missing service. Request denied."})
+
+    const result = await handler(buildEvent({body: JSON.stringify(requestWithMissingService)}))
+
+    expect(result.statusCode).toEqual(400)
+    expect(result.headers).toStrictEqual(expectedHeaders)
+    expect(result.body).toStrictEqual(expectedBody)
+  })
+
+  it("should return 400 when the vendor is missing", async () => {
+    const {vendor, ...requestWithMissingVendor} = request
+    const expectedBody = JSON.stringify({error: "Add photo request is missing vendor. Request denied."})
+
+    const result = await handler(buildEvent({body: JSON.stringify(requestWithMissingVendor)}))
+
+    expect(result.statusCode).toEqual(400)
+    expect(result.headers).toStrictEqual(expectedHeaders)
+    expect(result.body).toStrictEqual(expectedBody)
   })
 })
