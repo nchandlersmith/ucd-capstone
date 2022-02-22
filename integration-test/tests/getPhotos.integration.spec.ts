23c787853470b8ffe2729ed7422b0f9a7c127363
Nathan Chandler-Smith <nate.chandlersmith@gmail.com>
2022-02-22 15:44:01 -0600

    adds integration tests for auth

--- integration-test/tests/getPhotos.integration.spec.ts
+++ integration-test/tests/getPhotos.integration.spec.ts
@@ -7,6 +7,26 @@ describe("get photos persistence", () => {
   const photosUrl = "http://localhost:3000/dev/photos"
   const userId = "Integration Test User"
   const otherUserId = "Ghost Rider"
+  const myPhoto: PhotoDao = {
+    addedOn: DateTime.now().toISO(),
+    getPhotoUrl: "https://getMyPhoto.net",
+    photoId: "my photo id",
+    photoLabel: "my great photo",
+    putPhotoUrl: "https://putMyPhoto.io",
+    userId,
+    vendorId: "my vendor",
+    vendorService: "my service selection"
+  }
+  const yourPhoto: PhotoDao = {
+    addedOn: DateTime.now().toISO(),
+    getPhotoUrl: "https://getYourPhoto.biz",
+    photoId: "your photo id",
+    photoLabel: "your photo label",
+    putPhotoUrl: "https://putyourphoto.org",
+    userId: otherUserId,
+    vendorId: "your vendor",
+    vendorService: "your service selection"
+  }
 
   beforeAll(async () => {
     const dynamoResponse = await findAllPhotosByUserId(userId)
@@ -22,27 +42,21 @@ describe("get photos persistence", () => {
     }
   })
 
+  afterEach(async () => {
+    const dynamoResponse = await findAllPhotosByUserId(userId)
+    for (const item of dynamoResponse.Items) {
+      await deletePhotosByUserAndPhotoIds(item.photoId, userId)
+        .catch((error: any) => console.error(`Error occurred while cleaning up dynamo${error.message}`))
+    }
+
+    const dynamoResponse2 = await findAllPhotosByUserId(otherUserId)
+    for (const item of dynamoResponse2.Items) {
+      await deletePhotosByUserAndPhotoIds(item.photoId, otherUserId)
+        .catch((error: any) => console.error(`Error occurred while cleaning up dynamo${error.message}`))
+    }
+  })
+
   it("should return photos owned by specified user", async () => {
-    const myPhoto: PhotoDao = {
-      addedOn: DateTime.now().toISO(),
-      getPhotoUrl: "https://getMyPhoto.net",
-      photoId: "my photo id",
-      photoLabel: "my great photo",
-      putPhotoUrl: "https://putMyPhoto.io",
-      userId,
-      vendorId: "my vendor",
-      vendorService: "my service selection"
-    }
-    const yourPhoto: PhotoDao = {
-      addedOn: DateTime.now().toISO(),
-      getPhotoUrl: "https://getYourPhoto.biz",
-      photoId: "your photo id",
-      photoLabel: "your photo label",
-      putPhotoUrl: "https://putyourphoto.org",
-      userId: otherUserId,
-      vendorId: "your vendor",
-      vendorService: "your service selection"
-    }
     await putPhoto(myPhoto)
     await putPhoto(yourPhoto)
 
@@ -51,5 +65,30 @@ describe("get photos persistence", () => {
     expect(result.status).toEqual(201)
     expect(result.data.photos.length).toEqual(1)
     expect(result.data.photos[0]).toStrictEqual(myPhoto)
+  })
+
+  it("should not return photos when the auth header is missing", async () => {
+    await putPhoto(myPhoto)
+    await putPhoto(yourPhoto)
+
+    const result = await axios.get(photosUrl, {headers: {}}).catch(error => error)
+
+    expect(result.response.status).toEqual(403)
+    expect(Object.keys(result.response.headers)).toContain("access-control-allow-origin")
+    expect(result.response.headers["access-control-allow-origin"]).toEqual("*")
+    expect(result.response.data).toEqual({error: "Unauthorized user"})
+  })
+
+  it("should not return photos when user is unauthorized", async () => {
+    const invalidAuthToken = "sad day"
+    await putPhoto(myPhoto)
+    await putPhoto(yourPhoto)
+
+    const result = await axios.get(photosUrl, {headers: {Authorization: `Bearer ${invalidAuthToken}`}}).catch(error => error)
+
+    expect(result.response.status).toEqual(403)
+    expect(Object.keys(result.response.headers)).toContain("access-control-allow-origin")
+    expect(result.response.headers["access-control-allow-origin"]).toEqual("*")
+    expect(result.response.data).toEqual({error: "Unauthorized user"})
   })
 })
