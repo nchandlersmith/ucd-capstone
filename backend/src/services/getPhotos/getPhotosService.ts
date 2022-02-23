d7a58a624c1ccc78dce9d5365e29a7d44e4217f7
Nathan Chandler-Smith <nate.chandlersmith@gmail.com>
2022-02-22 19:41:03 -0600

    adds TODO

--- backend/src/services/getPhotos/getPhotosService.ts
+++ backend/src/services/getPhotos/getPhotosService.ts
@@ -1,6 +1,7 @@
 import {PhotoDao} from "../../models/photosModels";
 import {getPhotosByUser} from "../../persistence/dbClient";
 
+// TODO: should not return DAO
 export async function getPhotos(userId: string): Promise<PhotoDao[]> {
   return await getPhotosByUser(userId)
 }
