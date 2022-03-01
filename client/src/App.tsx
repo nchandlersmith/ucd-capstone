aefe274a31aab5d8cede11525e8de62a50d88045
Nathan Chandler-Smith <nate.chandlersmith@gmail.com>
2022-02-28 21:38:50 -0600

    client: adds accordion removes extra divs

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,19 @@
 import Photos from "./components/Photos";
+import {Accordion} from "react-bootstrap";
+import AccordionItem from "react-bootstrap/AccordionItem";
 
 function App() {
   return (
-    <div>
-      <Photos/>
-    </div>
+    <Accordion defaultActiveKey="0">
+      <AccordionItem eventKey="0">
+        <Accordion.Header>My Photos</Accordion.Header>
+        <Accordion.Body>Under Construction</Accordion.Body>
+      </AccordionItem>
+      <AccordionItem eventKey="1">
+        <Accordion.Header>Add Photo</Accordion.Header>
+        <Accordion.Body>{<Photos/>}</Accordion.Body>
+      </AccordionItem>
+    </Accordion>
   )
 }
 
--- client/src/components/Photos.tsx
+++ client/src/components/Photos.tsx
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import axios from 'axios';
 import Button from 'react-bootstrap/Button'
-import { Form } from 'react-bootstrap';
+import {Form} from 'react-bootstrap';
 
 function Photos() {
   const [user, setUser] = useState("")
@@ -10,45 +10,43 @@ function Photos() {
   const [service, setService] = useState("")
 
   return (
-    <div>
-      <Form>
-        <Form.Group controlId="user">
-          <Form.Label>User</Form.Label>
-          <Form.Control
-            type="email"
-            placeholder="user@example.com"
-            onChange={(event) => setUser(event.currentTarget.value)}/>
-          <Form.Text>Enter your email address.</Form.Text>
-        </Form.Group>
-        <Form.Group controlId="photo-label">
-          <Form.Label>Photo Label</Form.Label>
-          <Form.Control
-            type='text'
-            placeholder="Label your photo"
-            onChange={(event) => setLabel(event.currentTarget.value)}/>
-          <Form.Text>Enter a label for your photo for you and your vendor to refer.</Form.Text>
-        </Form.Group>
-        <Form.Group controlId="vendor">
-          <Form.Label>Vendor</Form.Label>
-          <Form.Control
-            type='text'
-            placeholder="Vendor goes here"
-            onChange={(event) => setVendor(event.currentTarget.value)}/>
-          <Form.Text>Enter the name of your vendor that will work on your photo.</Form.Text>
-        </Form.Group>
-        <Form.Group controlId="service">
-          <Form.Label>Service</Form.Label>
-          <Form.Control
-            type='text'
-            placeholder="Service goes here"
-            onChange={(event) => setService(event.currentTarget.value)}/>
-          <Form.Text>Enter the service that you want the vendor to perform on your photo.</Form.Text>
-        </Form.Group>
-        <Button variant='primary' type='reset' onSubmit={addPhoto} disabled={disableAddPhoto()  }>
-          Submit
-        </Button>
-      </Form>
-    </div>
+    <Form>
+      <Form.Group controlId="user">
+        <Form.Label>User</Form.Label>
+        <Form.Control
+          type="email"
+          placeholder="user@example.com"
+          onChange={(event) => setUser(event.currentTarget.value)}/>
+        <Form.Text>Enter your email address.</Form.Text>
+      </Form.Group>
+      <Form.Group controlId="photo-label">
+        <Form.Label>Photo Label</Form.Label>
+        <Form.Control
+          type='text'
+          placeholder="Label your photo"
+          onChange={(event) => setLabel(event.currentTarget.value)}/>
+        <Form.Text>Enter a label for your photo for you and your vendor to refer.</Form.Text>
+      </Form.Group>
+      <Form.Group controlId="vendor">
+        <Form.Label>Vendor</Form.Label>
+        <Form.Control
+          type='text'
+          placeholder="Vendor goes here"
+          onChange={(event) => setVendor(event.currentTarget.value)}/>
+        <Form.Text>Enter the name of your vendor that will work on your photo.</Form.Text>
+      </Form.Group>
+      <Form.Group controlId="service">
+        <Form.Label>Service</Form.Label>
+        <Form.Control
+          type='text'
+          placeholder="Service goes here"
+          onChange={(event) => setService(event.currentTarget.value)}/>
+        <Form.Text>Enter the service that you want the vendor to perform on your photo.</Form.Text>
+      </Form.Group>
+      <Button variant='primary' type='reset' onSubmit={addPhoto} disabled={disableAddPhoto()}>
+        Submit
+      </Button>
+    </Form>
   )
 
   async function addPhoto() {
