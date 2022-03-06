1cd60ec79e42770b12919167a8240fa570c780b0
Nathan Chandler-Smith <nate.chandlersmith@gmail.com>
2022-03-06 13:31:57 -0600

    adds vendor service to add vendor form

---
+++ client/src/components/AddVendorForm/AddVendorForm.spec.tsx
@@ -0,0 +1,16 @@
+import {render, screen} from "@testing-library/react";
+import AddVendorForm from "./AddVendorForm";
+
+describe("<AddVendorForm/>", () => {
+  it("should allow vendor to enter their name", () => {
+    render(<AddVendorForm/>)
+
+    expect(screen.getByLabelText("Vendor Name")).toBeInTheDocument()
+  })
+
+  it("should allow user to enter the services they offer", () => {
+    render(<AddVendorForm/>)
+
+    expect(screen.getByLabelText("Vendor Services")).toBeInTheDocument()
+  })
+})
--- client/src/components/AddVendorForm/AddVendorForm.tsx
+++ client/src/components/AddVendorForm/AddVendorForm.tsx
@@ -1,5 +1,19 @@
+import {Form} from "react-bootstrap";
+
 function AddVendorForm(): JSX.Element {
-  return <div>Under Construction</div>
+  return (
+    <Form>
+      <Form.Group controlId="vendor-name">
+        <Form.Label>Vendor Name</Form.Label>
+        <Form.Control type="text"/>
+        <Form.Text>This is the name your customers will see.</Form.Text>
+    </Form.Group>
+      <Form.Group controlId="vendor-services">
+        <Form.Label>Vendor Services</Form.Label>
+        <Form.Control type="text"/>
+        <Form.Text>Your customer will select one of these</Form.Text>
+      </Form.Group>
+    </Form>)
 }
 
 export default AddVendorForm
