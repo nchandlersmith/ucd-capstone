59aa0af2093cd9ab4c03e4b751fd76b7a97103d5
Nathan Chandler-Smith <nate.chandlersmith@gmail.com>
2022-03-06 19:45:04 -0600

    AddVendorForm.tsx captures form values

--- client/src/components/AddVendorForm/AddVendorForm.spec.tsx
+++ client/src/components/AddVendorForm/AddVendorForm.spec.tsx
@@ -1,16 +1,29 @@
-import {render, screen} from "@testing-library/react";
+import {fireEvent, render, screen} from "@testing-library/react";
 import AddVendorForm from "./AddVendorForm";
 
 describe("<AddVendorForm/>", () => {
-  it("should allow vendor to enter their name", () => {
+  beforeEach(() => {
     render(<AddVendorForm/>)
+  })
 
+  it("should allow vendor to enter their name", () => {
     expect(screen.getByLabelText("Vendor Name")).toBeInTheDocument()
   })
 
   it("should allow vendor to enter the services they offer", () => {
-    render(<AddVendorForm/>)
-
     expect(screen.getByLabelText("Vendor Services")).toBeInTheDocument()
+  })
+
+  it("should capture all the services the vendor offers", () => {
+    const vendorServiceEntry = screen.getByLabelText("Vendor Services")
+    const addServiceButton = screen.getByRole("button", {name: "Add to list"})
+
+    fireEvent.change(vendorServiceEntry, {target: {value: "Premium"}})
+    fireEvent.click(addServiceButton)
+    fireEvent.change(vendorServiceEntry, {target:{value: "Value"}})
+    fireEvent.click(addServiceButton)
+
+    expect(screen.getByText("Premium")).toBeInTheDocument()
+    expect(screen.getByText("Value")).toBeInTheDocument()
   })
 })
--- client/src/components/AddVendorForm/AddVendorForm.tsx
+++ client/src/components/AddVendorForm/AddVendorForm.tsx
@@ -1,6 +1,11 @@
-import {Form} from "react-bootstrap";
+import {Form, ListGroup, ListGroupItem} from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+import {useState} from "react";
 
 function AddVendorForm(): JSX.Element {
+  const [serviceToAdd, setServiceToAdd] = useState<string>("")
+  const [services, setServices] = useState<string[]>([])
+
   return (
     <Form>
       <Form.Group controlId="vendor-name">
@@ -8,12 +13,28 @@ function AddVendorForm(): JSX.Element {
         <Form.Control type="text"/>
         <Form.Text>This is the name your customers will see.</Form.Text>
     </Form.Group>
+    <ListGroup>
+    </ListGroup>
     <Form.Group controlId="vendor-services">
       <Form.Label>Vendor Services</Form.Label>
-      <Form.Control type="text"/>
-      <Form.Text>Your customer will select one of these</Form.Text>
+      <ListGroup>
+        {services.map((service, index) => (<ListGroupItem key={index}>{service}</ListGroupItem>))}
+      </ListGroup>
+      <Form.Control
+        type="text"
+        placeholder="Enter additional service here"
+        value={serviceToAdd}
+        onChange={(event) => setServiceToAdd(event.currentTarget.value)}/>
     </Form.Group>
+    <Button onClick={updateServices}>Add to list</Button>
     </Form>)
+
+  function updateServices() {
+    const newServicesList = services
+    newServicesList.push(serviceToAdd)
+    setServices(newServicesList)
+    setServiceToAdd("")
+  }
 }
 
 export default AddVendorForm
