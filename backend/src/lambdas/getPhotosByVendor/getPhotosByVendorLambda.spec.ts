4eb4ad86b1984ec3c6ad56535b05471574d84627
Nathan Chandler-Smith <nate.chandlersmith@gmail.com>
2022-03-20 09:26:23 -0500

    getPhotosByVendorLambda.ts calls the service and handles some sad paths

--- backend/src/lambdas/getPhotosByVendor/getPhotosByVendorLambda.spec.ts
+++ backend/src/lambdas/getPhotosByVendor/getPhotosByVendorLambda.spec.ts
@@ -3,14 +3,20 @@ import {PhotoByVendor} from "../../models/photosModels";
 import {handler} from "./getPhotosByVendorLambda";
 import {buildEvent} from "../../testUtils/eventUtils";
 
-jest.mock("dbClient", () => {
+jest.mock("../../persistence/dbClient", () => {
   return {
     getPhotosByVendor: jest.fn()
   }
 })
 
 describe("getPhotosByVendorLambda", () => {
+  const requiredHeaders = {
+    "access-control-allow-origin": "*"
+  }
+
   it("should return photos for specified vendor", async () => {
+    const userId = "test user"
+    const authHeader = {Authorization: `Bearer blarg-${userId}`}
     const expectedPhotos:PhotoByVendor[] = [{
       vendorName: "Some Vendor",
       vendorService: "Some service",
@@ -20,6 +26,31 @@ describe("getPhotosByVendorLambda", () => {
     }];
     (getPhotosByVendor as jest.Mock).mockImplementation(() => Promise.resolve(expectedPhotos))
 
+    const result = await handler(buildEvent({headers: authHeader}))
+
+    expect(result.statusCode).toEqual(200)
+    expect(result.headers).toStrictEqual(requiredHeaders)
+    expect(result.body).toStrictEqual(JSON.stringify(expectedPhotos))
+  })
+
+  it("should reject requests missing auth header", async () => {
+    const expectedErrorMessage = "Unauthorized User"
+
     const result = await handler(buildEvent())
+
+    expect(result.statusCode).toEqual(403)
+    expect(result.headers).toStrictEqual(requiredHeaders)
+    expect(result.body).toStrictEqual(JSON.stringify({error: expectedErrorMessage}))
+  })
+
+  it("should reject request from unauthorized user", async () => {
+    const expectedErrorMessage = "Unauthorized User"
+    const authHeader = {Authorization: "Bearer invalid user"}
+
+    const result = await handler(buildEvent({headers: authHeader}))
+
+    expect(result.statusCode).toEqual(403)
+    expect(result.headers).toStrictEqual(requiredHeaders)
+    expect(result.body).toStrictEqual(JSON.stringify({error: expectedErrorMessage}))
   })
 })
--- backend/src/lambdas/getPhotosByVendor/getPhotosByVendorLambda.ts
+++ backend/src/lambdas/getPhotosByVendor/getPhotosByVendorLambda.ts
@@ -1,10 +1,29 @@
 import {APIGatewayProxyEvent, APIGatewayProxyResult} from "aws-lambda";
+import {getPhotosByVendorService} from "../../services/getPhotosByVendorService/getPhootosByVendorService";
+import {authorize} from "../../utils/authUtils";
 
 export const handler = async  (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   const vendor = event.pathParameters?.vendorName
+  const authHeader = event.headers.Authorization
+  if (authHeader === undefined) {
+    return {
+      statusCode: 403,
+      headers: {"access-control-allow-origin": "*"},
+      body: JSON.stringify({error: "Unauthorized User"})
+    }
+  }
+  if (!authHeader.split(" ")[1].includes("blarg-")) {
+    return {
+      statusCode: 403,
+      headers: {"access-control-allow-origin": "*"},
+      body:JSON.stringify({error: "Unauthorized User"})
+    }
+  }
+  const photos = await getPhotosByVendorService("")
   return {
     statusCode: 200,
-    body: `{pathParams: ${vendor}}`
+    headers: {"access-control-allow-origin": "*"},
+    body: JSON.stringify(photos)
   }
 }
 
