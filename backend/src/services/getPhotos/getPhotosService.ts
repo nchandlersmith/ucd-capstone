e2ae6eee124efef2a2a24f606e7b1b9c658c86df
Nathan Chandler-Smith <nate.chandlersmith@gmail.com>
2022-03-19 19:11:40 -0500

    fixes TODO

--- backend/src/services/getPhotos/getPhotosService.ts
+++ backend/src/services/getPhotos/getPhotosService.ts
@@ -1,7 +1,6 @@
 import {PhotoData} from "../../models/photosModels";
 import {getPhotosByUser} from "../../persistence/dbClient";
 
-// TODO: should not return DAO
 export async function getPhotos(userId: string): Promise<PhotoData[]> {
   return await getPhotosByUser(userId)
 }
